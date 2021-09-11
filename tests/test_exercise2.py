7a4a3cd2224e98acf6af74c92c2a8290e654c6be
Angelica <Angelica137@users.noreply.github.com>
2021-09-11 20:11:52 +0100

    add rest of test cases, red

--- tests/test_exercise2.py
+++ tests/test_exercise2.py
@@ -5,5 +5,21 @@ def test_staircase_d_n_1_returns_1():
     assert staircase_d(1) == 1
 
 
-def test_staircase_d_n_1_returns_1():
+def test_staircase_d_n_2_returns_2():
     assert staircase_d(2) == 2
+
+
+def test_staircase_d_n_3_return_4():
+    assert staircase_d(3) == 4
+
+
+def test_staircase_d_4_return_7():
+    assert staircase_d(4) == 7
+
+
+def test_staircase_d_5_return_13():
+    assert staircase_d(5) == 13
+
+
+def test_staircase_d_20_return_121415():
+    assert staircase_d(20) == 121415
