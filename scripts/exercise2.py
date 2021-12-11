cc6b55110d5cc873308b5fb06e04b76ea2becce5
Angelica <Angelica137@users.noreply.github.com>
2021-12-11 19:55:04 +0000

    last update

--- scripts/exercise2.py
+++ scripts/exercise2.py
@@ -1,6 +1,14 @@
 def staircase_d(n):
     cache = []
     if n == 1:
+        cache.append(1)
         return 1
     elif n == 2:
+        cache.append(2)
         return 2
+    elif n == 3:
+        cache.append(4)
+        return 4
+    else:
+        cache.append(cache[-1] + cache[-2] + cache[-3])
+        return cache[-1]
